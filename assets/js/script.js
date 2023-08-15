04cd8e8f5f9325ccd5c5199b8f6b9c1354552481
Soleiles <ktaing789@gmail.com>
2023-08-15 19:14:24 -0400

    set up apikey and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,19 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+// ****** Key and Elements ******
+const APIKey = "9f776f17932acae3940eefa178872e8c";
+let forecastCount;
+let weatherIcon;
+let longitude, latitude;
+let cityHistory = [];
+
+const searchBtn = $(".btn");
+const clearBtn = $(".clear-btn");
+let searchHistory = $(".search-history");
+let currentCity = $(".current-city");
+let currentDate = $(".current-date");
+let currentTemp = $(".current-temp");
+let currentWind = $(".current-wind");
+let currentHumidity = $(".current-humidity");
