7b415de43466d4a5dfe87eb414fe8765bbd99d09
Soleiles <ktaing789@gmail.com>
2023-08-15 19:48:20 -0400

    working search api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,62 @@ let currentDate = $(".current-date");
 let currentTemp = $(".current-temp");
 let currentWind = $(".current-wind");
 let currentHumidity = $(".current-humidity");
+
+let currentMonth, currentDay, currentYear, fullDate, unixDate, convertedDate, currentTime;
+
+
+function convertUnix(unixDate) {
+    currentTime = new Date();
+    convertedDate = new Date(unixDate*1000 + currentTime.getTimezoneOffset() * 60 * 1000);
+    currentMonth = convertedDate.getMonth() + 1;
+    currentDay = convertedDate.getDate();
+    currentYear = convertedDate.getFullYear();
+    fullDate = `${currentMonth}/${currentDay}/${currentYear}`
+    return fullDate;
+};
+
+function init() {
+    var historyTemp = localStorage.getItem("cityHistory");
+    if(historyTemp){
+        cityHistory = JSON.parse(historyTemp);
+    }
+    var searchTemp = localStorage.getItem("search");
+    if(searchTemp){
+        getCurrentWeather(searchTemp);
+    } else {
+        citySearch = "Philadelphia"
+        queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey + "&units=imperial";
+        getCurrentWeather(queryURL);
+    }
+};
+
+init();
+
+function getCurrentWeather(queryURL) {
+    localStorage.setItem("search", queryURL);
+    fetch(queryURL).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        latitude = data.coord.lat;
+        longitude = data.coord.lon;
+        weatherIcon = data.weather[0].icon;
+        unixDate = data.dt;
+        currentCity.text(data.name);
+        $(".city-and-date").children(".icon").attr("src"), `https://openweathermap.org/img/wn/${weatherIcon}.png`;
+        currentDate.text(convertUnix(unixDate));
+        currentTemp.text(data.main.temp);
+        currentWind.text(data.wind.speed);
+        currentHumidity.text(data.main.humidity);
+    });
+};
+
+searchBtn.on("click", function() {
+    let userCity = $("#enter-city");
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity.val() + "&appid=" + APIKey + "&units=imperial";
+    cityToSearch = userCity.val();
+    if (userCity.val() !== "") {
+        getCurrentWeather(queryURL);
+    }
+    cityHistory.push(cityToSearch);
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+});
