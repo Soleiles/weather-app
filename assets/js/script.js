e3502f1f1a49185862e32ad47bb4f946d4fc72b2
Soleiles <ktaing789@gmail.com>
2023-08-15 20:56:56 -0400

    updated styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 // ****** Key and Elements ******
 const APIKey = "9f776f17932acae3940eefa178872e8c";
 let forecastCount;
@@ -97,8 +87,6 @@ function createHistoryButtons() {
             historyBtn.val(cityName);
             historyBtn.attr("class", "history-btn");
             searchHistory.append(historyBtn);
-            searchHistory.css("display", "flex");
-            searchHistory.css("flex-wrap", "wrap");
             
             addedValues.push(cityName);
         }
